94fb8c2803ed360bb7611d444a5d5f6d0613a9e1
stephane-mathieu <mathieu.stephane@laplateforme.io>
2021-11-02 12:09:14 +0100

    finaly02

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -5,43 +5,52 @@ $a = 1.23456789;
 $str = "Hello LaPlateforme!";
 $mybool2 = false;
 ?>
-<link rel="stylesheet" href="page.css" type="text/css">
-<html>
 
-<table>
-    <thead>
-    <tr>
-       <th>type</th>
-       <th>nom</th>
-       <th>valeur</th>
-   </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>bool</td>
-            <td>$mybool</td>
-            <td> <?php echo "$mybool"; ?></td>
-        </tr>
-        <tr>
-            <td>bool</td>
-            <td>$mybool2</td>
-            <td> <?php echo "0"; ?></td>
-        </tr>
-        <tr>
-            <td>int</td>
-            <td>$val</td>
-            <td><?php echo "$val"; ?></td>
-        </tr>
-        <tr>
-            <td>str</td>
-            <td>$str</td>
-            <td><?php echo "$str"; ?></td>
-        </tr>
-        <tr>
-            <td>int</td>
-            <td>$a</td>
-            <td><?php echo "$a"; ?></td>
-        </tr>
-    </tbody>
-</table>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="page.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <thead>
+            <tr>
+                <th>type</th>
+                <th>nom</th>
+                <th>valeur</th>
+            </tr>
+        </thead>
+        <tbody>
+             <tr>
+                <td>bool</td>
+                <td>$mybool</td>
+                <td> <?php echo "$mybool"; ?></td>
+            </tr>
+            <tr>
+                <td>bool</td>
+                <td>$mybool2</td>
+                <td> <?php echo "0"; ?></td>
+            </tr>
+            <tr>
+                <td>int</td>
+                <td>$val</td>
+                <td><?php echo "$val"; ?></td>
+            </tr>
+            <tr>
+                <td>str</td>
+                <td>$str</td>
+                <td><?php echo "$str"; ?></td>
+            </tr>
+            <tr>
+                <td>int</td>
+                <td>$a</td>
+                <td><?php echo "$a"; ?></td>
+            </tr>
+        </tbody>
+        </table>
+</body>
 </html>
