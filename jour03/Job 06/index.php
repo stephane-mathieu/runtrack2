f50c5310999070890cb940d903b11df57fd57c51
stephane-mathieu <mathieu.stephane@laplateforme.io>
2021-11-04 17:45:41 +0100

    correctionbug

--- jour03/Job 06/index.php
+++ jour03/Job 06/index.php
@@ -2,7 +2,7 @@
 
 function my_reverse_str(){
     $str = "Les choses que l'on
-    possede finissent par nous posseder";
+    possede finissent par nous posseder.";
     $i = -1;
     while ($str[$i] != NULL) {
         echo $str[$i];
